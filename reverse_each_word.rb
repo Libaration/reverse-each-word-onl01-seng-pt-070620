04a28d0b9e468c9b3fda4f699dc3bc0b9dfb6b8c
Cristian Deleon <libaration@gmail.com>
2020-07-07 18:24:28 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+myArray =[]
+my_statements=[]
+myArray.push sentence.reverse.split(" ")
+myArray.collect {|word| my_statements << word.reverse}
+
+
+ my_statements.join(",")
+end
+reverse_each_word("Testing thissss one")
